b3ad995c25d9c691a39dd1d918df5a4f7a7ad631
meyearwood <margoyearwood@gmail.com>
2017-11-14 00:38:46 -0500

    Fixing my boo-boo

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.json());
+// app.use(bodyParser.text());
+// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 //ROUTER
 require('./app/routing/apiRoutes.js')(app);
