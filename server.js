5290e6273a6d9aede0b2c30d8d2fbcd9f4bc6331
meyearwood <margoyearwood@gmail.com>
2017-11-14 00:46:26 -0500

    Fixing my boo-boo

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// app.use(bodyParser.text());
-// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 //ROUTER
 require('./app/routing/apiRoutes.js')(app);
